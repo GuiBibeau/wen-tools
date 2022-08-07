bb908ca25d8294178da18177109617c620580a3f
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-08-06 21:26:33 -0400

    formatting

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -8,38 +8,38 @@ import "../style.css";
 import { useDesiredChainId, useTheme } from "./ButtonProvider";
 
 const Skeleton = () => {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  return (
-    <button type="button" onClick={open} className={theme.button}>
-      <div className="w-32 bg-gray-100 h-6 rounded-md " />
-    </button>
-  );
+	return (
+		<button type="button" onClick={open} className={theme.button}>
+			<div className="w-32 bg-gray-100 h-6 rounded-md " />
+		</button>
+	);
 };
 
 export const Button = () => {
-  const [loading, setLoading] = React.useState(true);
-  const { metamaskPresent, address, chainId } = useWen();
-  const theme = useTheme();
-  const desiredChainId = useDesiredChainId();
+	const [loading, setLoading] = React.useState(true);
+	const { metamaskPresent, address, chainId } = useWen();
+	const theme = useTheme();
+	const desiredChainId = useDesiredChainId();
 
-  const wrongNetwork = `0x${desiredChainId.toString(16)}` !== chainId;
+	const wrongNetwork = `0x${desiredChainId.toString(16)}` !== chainId;
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 600);
-  }, [metamaskPresent]);
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 600);
+	}, [metamaskPresent]);
 
-  if (loading) {
-    return <Skeleton />;
-  }
+	if (loading) {
+		return <Skeleton />;
+	}
 
-  return (
-    <button type="button" onClick={open} className={theme.button}>
-      <div className="w-32 h-6">
-        {!address && "Connect"}
-        {address && wrongNetwork && "Wrong Network"}
-        {address && !wrongNetwork && getShortenedAddress(address)}
-      </div>
-    </button>
-  );
+	return (
+		<button type="button" onClick={open} className={theme.button}>
+			<div className="w-32 h-6">
+				{!address && "Connect"}
+				{address && wrongNetwork && "Wrong Network"}
+				{address && !wrongNetwork && getShortenedAddress(address)}
+			</div>
+		</button>
+	);
 };
